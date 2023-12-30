8104885d6dba8c4561d13528b21d72b2caaa8f54
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2023-12-30 21:51:59 +0530

    more

---
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -0,0 +1,29 @@
+class PrimeCalculator {
+
+    int nth(int n) throws IllegalArgumentException{
+        if(n==0)
+             throw new IllegalArgumentException();
+        int k=0;
+        int factor=0;
+        int prime=0;
+       while(n+1>=0)
+           {
+               for(int i=2;i<=Math.sqrt(k);i++)
+                   {
+                       if(k%i==0)
+                           factor++;
+                   }
+               if(factor==0)
+               {
+                   n--;
+                   prime=k;
+                
+                   
+               }
+               k++;
+               factor=0;
+           }
+        return prime;
+    }
+
+}
